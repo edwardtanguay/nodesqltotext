25364c52f2b7588390c3eafd12de63969145c5a6
Edward Tanguay (wk) <edward@tanguay.info>
2021-01-18 12:19:07 +0100

    writes to file

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('test');
+fs = require('fs');
+const SqliteManager = require('./sqliteManager');
+
+const sqliteManager = new SqliteManager('data/main.sqlite');
+
+sqliteManager.getRecordWithSql(`SELECT * FROM contacts WHERE id=1`)
+	.then((record) => {
+		if (record !== null) {
+			console.log(record);
+		}
+	})
+	.catch((error) => console.log(error));
+
+let content = '';
+sqliteManager.getRecordsWithSql(`SELECT * FROM contacts LIMIT 5`)
+	.then((records) => {
+		for (const record of records) {
+			console.log(record.firstName);
+			content += record.firstName + '\n';
+		}
+		fs.writeFileSync('output/contacts.txt', content);
+	})
+	.catch((error) => console.log(error));
---
+++ sqliteManager.js
@@ -0,0 +1,42 @@
+const sqlite3 = require('sqlite3').verbose();
+ 
+class SqliteManager {
+    constructor(dbPathAndFileName) {
+        this.dbPathAndFileName = dbPathAndFileName;
+    }
+ 
+    getRecordWithSql(sql) {
+        return new Promise((resolve, reject) => {
+            const db = new sqlite3.Database(this.dbPathAndFileName);
+            db.all(sql, function (err, records) {
+                if (records === undefined) {
+                    reject(err);
+                } else if (records.length === 0) {
+                    resolve(null);
+                } else {
+                    resolve(records[0]);
+                }
+            });
+            db.close();
+        });
+    }
+ 
+    getRecordsWithSql(sql) {
+        return new Promise((resolve, reject) => {
+            const db = new sqlite3.Database(this.dbPathAndFileName);
+            db.all(sql, function (err, records) {
+                if (records === undefined) {
+                    reject(err);
+                } else if (records.length === 0) {
+                    resolve([]);
+                } else {
+                    resolve(records);
+                }
+            });
+            db.close();
+        });
+    }
+ 
+}
+ 
+module.exports = SqliteManager;
